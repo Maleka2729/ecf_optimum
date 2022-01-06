e326baa5cb7661e4c2804b9dad773678346d9827
Malekalita TAUGAMOA <t.malekalita@gmail.com>
2022-01-06 22:23:35 +1100

    commit access db

--- wp-config.php
+++ wp-config.php
@@ -25,10 +25,10 @@
 define( 'DB_NAME', 'optimum' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'malekalita' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'secret' );
+define( 'DB_PASSWORD', 'devweb2022' );
 
 /** Adresse de l’hébergement MySQL. */
 define( 'DB_HOST', 'localhost' );
